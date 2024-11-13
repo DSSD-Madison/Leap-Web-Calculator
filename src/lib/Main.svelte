26634b68c29f740e5cef622b710a8ae1b8912519
Tanvi Raman <tanviraman@Tanvis-MacBook-Pro.local>
2024-11-13 00:54:36 -0600

    adding files for output component and main framework

---
+++ src/lib/Main.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Control from './Control.svelte';
+    import OutputComponent from './OutputComponent.svelte';
+  </script>
+  
+  <main>
+    <!-- Display the Control component -->
+    <Control />
+  
+    <!-- Display the OutputComponent component -->
+    <OutputComponent />
+  </main>
+  
+  <style>
+    main {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 1rem;
+    }
+    :global(body) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    background-color: #f0f0f0; /* Light gray background for the entire page */
+    }
+  </style>
---
+++ src/lib/OutputComponent.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let estimatedCost;
+  </script>
+  
+  <div class="estimated-cost">
+    <p>ESTIMATED COSTS:<br /></p>
+    <p>$<span>{estimatedCost}</span></p>
+  </div>
+  
+  <style>
+    .estimated-cost {
+      background-color: #007bff;
+      border-radius: 10px;
+      padding: 10px;
+      text-align: center;
+      font-weight: bold;
+      color: white;
+      width: 350px;
+    }
+  
+    p {
+      margin: 0;
+    }
+  </style>
